77c175bb30039ba8435da0b651afaca7d5321c39
e1730934 <e1730934@cmaisonneuve.qc.ca>
2022-02-12 21:20:37 -0500

    Avancement du backend.

---
+++ frontend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/inscription", (req,res) =>{
+    res.sendFile(__dirname +"/public/forms/signup/signup.html")
+})
+
+app.get("/connexion", (req,res) =>{
+    res.sendFile(__dirname +"/public/forms/login/login.html")
+})
+
+app.listen(PORT, () => {
+    console.log(`Mon application frontale roule sur http://localhost:${PORT}`);
+})
